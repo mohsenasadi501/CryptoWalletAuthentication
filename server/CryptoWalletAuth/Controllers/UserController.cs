245e8f11b86a4d5e961f3e131893787c5a5c92f7
mohsen <mohsenasadi501@yahoo.com>
2022-06-02 15:16:30 +0430

    add efcore to api project

---
+++ server/CryptoWalletAuth/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoWalletAuth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public Int64 GetNonce()
+        {
+            _dataContext.Users.ToList();
+            return 1;
+        }
+    }
+}
---
+++ server/CryptoWalletAuth/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CryptoWalletAuth
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? PublicAddress { get; set; }
+        public Int64 Nonce { get; set; }
+    }
+}

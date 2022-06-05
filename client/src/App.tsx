0f9fdb8aecf843a65207e6e4293a8ff0a03ee430
mohsen <mohsenasadi501@yahoo.com>
2022-06-05 23:55:07 +0430

    add web3model and etherjs to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,48 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
 
 function App() {
-  const baseURL = "https://localhost:7061/User/nonce";
+  const baseURL = "https://localhost:7061/";
   const [nonce, setNonce] = React.useState("");
+  const [token, setToken] = React.useState("");
+  const [account, setAccount] = React.useState();
 
+  async function connect() {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+  }
   function getNonnce() {
+    let uri = baseURL + "User/nonce";
     axios
-      .get(baseURL, {
+      .get(uri, {
         params: { publicAddress: "0x174A639d18a2EE6590ed1A201F8CCC76A52FFB13" },
       })
       .then((response) => {
-        console.log(response)
+        console.log(response);
         setNonce(response.data);
+      });
+  }
+  function login() {
+    let uri = baseURL + "User/authenticate";
+    const json = JSON.stringify({
+      publicAddress: "0x174A639d18a2EE6590ed1A201F8CCC76A52FFB13",
+      signature: nonce.toString(),
+    });
+    axios
+      .post(uri, json, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        localStorage.setItem("token", response.data.accessToken);
+        localStorage.setItem("refreshToken", response.data.accessToken);
+        setToken(response.data.accessToken);
       });
   }
 
@@ -22,12 +51,30 @@ function App() {
       <div className="row">
         <div className="col">
           <br></br>
-          <button type="button" className="btn btn-primary" onClick={getNonnce}>
-            Get Nonce
-          </button>
-          <input type="text" value={nonce}></input>
+          <div className="input-group mb-3">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={getNonnce}
+            >
+              Get Nonce
+            </button>
+            <input type="text" value={nonce}></input>
+            <br></br>
+          </div>
+          <div className="input-group mb-3">
+            <button type="button" className="btn btn-primary" onClick={login}>
+              Login
+            </button>
+            <input type="text" value={token}></input>
+          </div>
         </div>
         <div className="col"></div>
+        <div className="input-group mb-3">
+            <button type="button" className="btn btn-primary" onClick={connect}>
+              Connect to MetaMask
+            </button>
+          </div>
         <div className="col"></div>
       </div>
     </div>
